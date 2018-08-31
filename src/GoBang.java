c3dd70713ca255d45678cbc407890e341cc461ea
MXuDong <2941884109@qq.com>
2018-08-31 17:28:19 +0800

    Game Windows Can show

--- src/GoBang.java
+++ src/GoBang.java
@@ -1,10 +1,13 @@
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GoBang {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GameBackGround gbg = new GameBackGround();
 	}
 
 }
@@ -19,8 +22,49 @@ class GameBackGround extends JFrame {
 //	设置棋盘大小:Weigh-宽 、Heigh-高
 	private static final byte GAME_BACK_GROUND_WEIGH = 19;
 	private static final byte GAME_BACK_GROUND_HEIGH = 19;
+//	设置游戏区域大小
+	private static final int GAME_PANEL_WEIGH = 475;
+	private static final int GAME_PANEL_HEIGH = 470;
+//	设置游戏窗口大小
+	private static final int GAME_WINDOWS_WEIGH = 900;
+	private static final int GAME_WINDOWS_HEIGH = 600;
 	
+	//游戏窗口
+	private JFrame GameWindow;
 	
+	//静态字符串
+	private static final String WINDOW_TITLE = "五子棋";
+	
+//	构造
+	public GameBackGround() {
+		GameWindow = new JFrame();
+		GameWindow.setTitle(WINDOW_TITLE);
+		GameWindow.setSize(GAME_WINDOWS_WEIGH, GAME_WINDOWS_HEIGH);
+		GameWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		GameWindow.setVisible(true);
+	}
+	
+	/**
+	 * 棋盘图像绘制类
+	 */
+	class GamePanel extends JPanel{
+
+		@Override
+		protected void paintComponent(Graphics arg0) {
+			// TODO Auto-generated method stub
+			super.paintComponent(arg0);
+			
+			int row, line;
+			
+			for(row = 0; row < GameBackGround.GAME_BACK_GROUND_WEIGH; row++) {
+				arg0.drawLine(0, 25*row, 475, 25*row);
+			}
+			for(line = 0; line < GameBackGround.GAME_BACK_GROUND_HEIGH; line++) {
+				arg0.drawLine(25*line, 0, 25*line, 475);
+			}
+		}
+		
+	}
 
 	/**
 	 * 虚拟棋盘类 属性：棋盘
